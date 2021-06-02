4b82bd1645953d98e2673cc4e53666c1015bd6ed
Sarah George <sarahgeorge1394@gmail.com>
2021-06-02 14:57:53 -0400

    Add project files.

---
+++ PoWebApi/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoWebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } //primary key
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public bool IsManager { get; set; } //will default to false when not stated other wise
+
+        public Employee() { } //constructor, probably dont need but doesnt hurt to have
+
+    }
+}
