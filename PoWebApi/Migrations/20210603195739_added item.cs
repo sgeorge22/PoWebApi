c15fd2983bdbe0c678d007083c5c4b04edf0fdd0
Sarah George <sarahgeorge1394@gmail.com>
2021-06-03 16:06:14 -0400

    Added an Items Model and Controller

---
+++ PoWebApi/Migrations/20210603195739_added item.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PoWebApi.Migrations
+{
+    public partial class addeditem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(type: "decimal (9,2)", nullable: false),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
---
+++ PoWebApi/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoWebApi.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required, Column(TypeName = "decimal (9,2)")]
+        public decimal Price { get; set; }
+        [Required]
+        public bool Active { get; set; } = true;
+
+
+    }
+}
